63b10f8c65907fd59d8ef69b93300bfe6e38868e
Ethan Soo <ethan.soo@hotmail.com>
2020-11-27 22:55:19 -0800

    Add files via upload
    writing/reading data to/from text file instead of storing in the stack

--- story_bot.py
+++ story_bot.py
@@ -5,13 +5,50 @@ import os
 TOKEN = os.environ['DISCORD_TOKEN']
 
 client = discord.Client()
-
+path = "data.txt"
 word_list = []
-story_active = False
+
+def file_is_empty():
+    global path
+    file = open(path, "r")
+    try:
+        data = file.readline()
+        return data == ""
+    finally:
+        file.close()
+
+def clear_file():
+    global path
+    #clear file
+    file = open("data.txt", "w")
+
+def write_to_file(data):
+    global path
+    file = open(path, "w")
+    try:
+        file.write(data)
+    finally:
+        file.close()
+
+def append_to_file(data):
+    global path
+    file = open(path, "a")
+    try:
+        file.write(data)
+    finally:
+        file.close()
+
+def read_file():
+    global path
+    file = open(path, "r")
+    try:
+        return file.readline()
+    finally:
+        file.close()
 
 @client.event
 async def on_message(message):
-    global word_list, story_active
+    global word_list
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
@@ -19,26 +56,42 @@ async def on_message(message):
     if message.content.startswith('!story help'):
         msg = 'How to use:\n!begin story:   Starts a story!\n!add <enter word(s) here>:   Adds new words to the story!\n!end story:   Ends the story!'
         await message.channel.send(msg)
+
     elif message.content.startswith('!begin story'):
+        clear_file()
         word_list = ['Once', 'upon', 'a', 'time,']
         msg = 'Let the story begin...'
-        story_active = True
-        await message.channel.send(msg)
-    elif story_active and message.content.startswith('!add'):
-        msg = message.content
-        if msg.startswith('!add ') == False:
-            msg = msg[:4] + ' ' + msg[4:]
-
-        word_list.extend(msg.split(' ')[1:])
-        msg = " ".join(word_list)
         await message.channel.send(msg)
 
-    elif story_active and message.content.startswith('!end story'):
-        msg = 'And that\'s the end of the story! This was what the story was about:'
-        await message.channel.send(msg)
         msg = " ".join(word_list)
-        story_active = False
-        await message.channel.send(msg)
+        write_to_file(msg)
+
+    elif message.content.startswith('!add'):
+        #if story has not started
+        if (file_is_empty()):
+            await message.channel.send("The story has not begun!")
+        else:
+            msg = message.content
+            if msg.startswith('!add ') == False:
+                msg = msg[:4] + ' ' + msg[4:]
+
+            word_list = msg.split(' ')[1:]
+            msg = " ".join(word_list)
+            append_to_file(" " + msg)
+            data = read_file()
+            await message.channel.send(data)
+
+    elif message.content.startswith('!end story'):
+        #if story has not started
+        if (file_is_empty()):
+            await message.channel.send("The story has not begun!")
+        else:
+            msg = 'And that\'s the end of the story! This was what the story was about:'
+            await message.channel.send(msg)
+            data = read_file()
+            await message.channel.send(data)
+            clear_file()
+
 @client.event
 async def on_ready():
     print('Logged in as')
